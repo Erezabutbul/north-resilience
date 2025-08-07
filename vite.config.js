868f4beea3b0fd81a35d2d3047f2824b7e24141e
Erezabutbul <63152367+Erezabutbul@users.noreply.github.com>
2025-08-07 16:05:30 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/north-resilience-ae2e2c0c/',
   server: {
     allowedHosts: true
   },
